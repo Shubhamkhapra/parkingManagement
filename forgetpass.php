a025b72462a5b7a6992c29fa5aa6dd1cae22d8f6
ankita <ankitaattri53@gmail.com>
2022-09-15 18:48:25 +0530

    Changes(15 Sep,2022, 06:48PM)

--- forgetpass.php
+++ forgetpass.php
@@ -5,20 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/forgetpass.css">
+    <link rel="stylesheet" href="css/forgetpass.css">
     <script src="https://kit.fontawesome.com/ef417ecd31.js" crossorigin="anonymous"></script>
     <title>Forget Password</title>
     <?php
-    include './config/connection.php';
+    include './config/connection.php';   //Connection file
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
      $email = $_POST['email'];
      $phone= $_POST['phone'];
+     //Comparison of input from database
      $q = "SELECT * from `tbl_admin` where `email`='$email' ";
      $result = mysqli_query($conn, $q);
      $row = mysqli_fetch_array($result);
      if ($row[3] == $phone) 
      {
+        //Changing password in database
         $password = $_POST["pass"];
         $qu="UPDATE `tbl_admin` SET Password= '$password' where `email`='$email' ";
         $res = mysqli_query($conn, $qu);
@@ -42,14 +44,18 @@
 </head>
 
 <body>
+    <!----Main div ----->
     <div class="login-div">
         <form method="post" autocomplete='off' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <!----Div for logo----->
             <div class="logo">
                 <div class="logoimage">
-                    <img src="logo.png" alt="Parking Logo">
+                    <img src="images/loggo.jpeg" alt="Parking Logo">
                 </div>
             </div>
+            <!------Title------>
             <div class="title">Forgot Password?</div>
+            <!---Div for input---->
             <div class="fields">
                 <div class="email"><i class="fa-regular fa-envelope"></i><input type="email" name='email' class="user-email" placeholder="email" required />
                 </div>
--- index.php
+++ index.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="css/index.css">
     <script src="https://kit.fontawesome.com/ef417ecd31.js" crossorigin="anonymous"></script>
-    <title>Index</title>
+    <title>Login</title>
+    <!----PHP Connectivity ----->
     <?php
-    include './config/connection.php';
+    include './config/connection.php';  //connection file
     $emailErr = $passErr = "";
+    //Validations for input fields
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
         $pass = $_POST['pass'];
@@ -37,6 +39,7 @@
                 $passErr = "Invalid";
             }
         }
+        //Comparision of input with database
         $q = "SELECT * from `tbl_admin` where `email`='$email' ";
         $result = mysqli_query($conn, $q);
         $row = mysqli_fetch_array($result);
@@ -56,14 +59,18 @@
 </head>
 
 <body>
+    <!----------Main Div ----------->
     <div class="login-div">
         <form method="post" autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <!--------Div For logo ------>
             <div class="logo">
                 <div class="logoimage">
                     <img src="./images/loggo.jpeg" alt="Parking Logo">
                 </div>
             </div>
-            <div class="title">Index</div>
+            <!------Title -------->
+            <div class="title">Login</div>
+            <!-----Input fields ----->
             <div class="fields">
                 <div class="email"><i class="fa-regular fa-envelope"></i><input type="email" name='email' class="user-email" placeholder="email" required />
                     <span><?php echo $emailErr ?></span>
@@ -73,6 +80,7 @@
                 </div>
                 <button class="signin-button" name='submit'>Login</button>
             </div>    
+            <!------Forgot password-------->
                 <div class="link">
                     <a  class='anchor' href="forgetpass.php" >Forgot password?</a>
                 </div>
